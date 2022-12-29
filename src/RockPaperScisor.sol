53f140111fb4ac9edf5d2baeec488d3e09444561
Théo RICHARD <mail@rcode.tech>
2022-12-29 11:33:25 +0100

    feat: commit logic for 2 players

---
+++ src/RockPaperScisor.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+// solhint-disable-next-line no-empty-blocks
+contract RockPaperScisor {
+    enum Action {
+        ROCK,
+        PAPER,
+        SCISOR
+    }
+
+    enum Stage {
+        COMMIT,
+        REVEAL
+    }
+
+    Stage public stage = Stage.COMMIT;
+    uint8 public playersCounter;
+
+    uint256 public depositedETH;
+    mapping(address => bytes32) public commitOf;
+    function commitOnlyTwoPlayers(bytes32 data, uint256 salt) public payable {
+        require(commitOf[msg.sender] == bytes32(""), "Already commited");
+        require(stage == Stage.COMMIT, "Two players already competing");
+        require(msg.value == 5 ether, "Deposit 5 ether");
+
+        ++playersCounter;
+        commitOf[msg.sender] = keccak256(abi.encodePacked(data, salt));
+        depositedETH += msg.value;
+
+        if (playersCounter == 2) stage = Stage.REVEAL;
+    }
